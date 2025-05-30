ff6f8bf99ae9f42b75ddb7f5437f50d84e016cd5
mickeyescaleto <mickeyescaleto@gmail.com>
2025-05-30 16:58:00 +0300

    feat: create nuxt application

---
+++ nuxt.config.ts
@@ -0,0 +1,44 @@
+import { defineNuxtConfig } from 'nuxt/config';
+
+export default defineNuxtConfig({
+  compatibilityDate: '2025-05-30',
+  devtools: { enabled: true },
+  alias: {
+    '@': '../src',
+  },
+  dir: {
+    pages: './src/app/routes',
+  },
+  hooks: {
+    'app:templates'(app) {
+      app.rootComponent = '@/app/app.vue';
+    },
+  },
+  app: {
+    rootTag: 'main',
+    rootAttrs: {
+      id: 'application',
+    },
+    head: {
+      noscript: [{ textContent: 'JavaScript is required' }],
+      htmlAttrs: { lang: 'en' },
+      charset: 'utf-8',
+      viewport: 'width=device-width, initial-scale=1',
+      title: 'Artem Kuznetsov • @mickeyescaleto',
+      meta: [
+        {
+          name: 'description',
+          content:
+            'Web developer specializing in React, VueJS, NextJS and NuxtJS',
+        },
+      ],
+      link: [
+        {
+          rel: 'icon',
+          type: 'image/x-icon',
+          href: '/favicon.ico',
+        },
+      ],
+    },
+  },
+});
